1e8ec113f6ae22aa115e8b6c45d9ef5ccf3472b0
Rohit <rohit9075@gmail.com>
2018-05-02 08:51:03 +0530

    fibonacci series number

---
+++ src/fibonacci_series/FibonacciSeries.java
@@ -0,0 +1,31 @@
+package fibonacci_series;
+
+public class FibonacciSeries {
+
+    // Driver main method to run the program
+    public static void main(String args[])
+    {
+        // Variable declaration and Initialization
+        int mFirstNumber=0,mSecondNumber =1, mThirdNumber ,i,count=10;
+
+        //printing 0 and 1
+        System.out.print(mFirstNumber+" "+mSecondNumber);
+
+        //loop starts from 2 because 0 and 1 are already printed
+        for(i=2;i<count;++i)
+        {
+            // Calculating third number by adding first two number
+            mThirdNumber = mFirstNumber + mSecondNumber;
+
+            // Printing third number onwards
+            System.out.print(" "+mThirdNumber);
+
+            // Assigning second number to firstNumber
+            mFirstNumber = mSecondNumber;
+
+            // Assigning third number to SecondNumber
+            mSecondNumber = mThirdNumber;
+        }
+
+    }
+}
