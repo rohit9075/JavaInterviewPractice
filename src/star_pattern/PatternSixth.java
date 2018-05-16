3e662cf4f377a8c0c9272a6c1a14a970e222a4ad
Rohit Kumar <rohit9075@gmail.com>
2018-05-16 09:36:03 +0530

    commit

--- src/star_pattern/PatternSixth.java
+++ src/star_pattern/PatternSixth.java
@@ -1,4 +1,65 @@
 package star_pattern;
 
+/*
+Java program to print the Star pattern like -
+
+        *
+       * *
+      * * *
+     * * * *
+    * * * * *
+    * * * * *
+     * * * *
+      * * *
+       * *
+        *
+*/
+
 public class PatternSixth {
+
+    // Prints diamond pattern with 2n rows
+        private static void printDiamond(int n)
+        {
+            int space = n - 1;
+
+            // run loop (parent loop) till number  of rows
+            for (int i = 0; i < n; i++)
+            {
+                // loop for initially space, before star printing
+                for (int j = 0; j < space; j++)
+                    System.out.print(" ");
+
+                // Print i+1 stars
+                for (int j = 0; j <= i; j++)
+                    System.out.print("* ");
+
+                System.out.print("\n");
+                space--;
+            }
+
+            // Repeat again in reverse order
+            space = 0;
+
+            // run loop (parent loop) till number of rows
+            for (int i = n; i > 0; i--)
+            {
+                // loop for initially space, before star printing
+                for (int j = 0; j < space; j++)
+                    System.out.print(" ");
+
+                // Print i stars
+                for (int j = 0; j < i; j++)
+                    System.out.print("* ");
+
+                System.out.print("\n");
+                space++;
+            }
+        }
+
+        /* Driver program to test above Method */
+        public static void main(String[] args)
+        {
+            printDiamond(5);
+
+        }
 }
