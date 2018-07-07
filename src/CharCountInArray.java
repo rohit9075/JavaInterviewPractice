37b7be5d60ef9d05b7630d6c38d3fdd738b84009
Rohit Kumar <rohit9075@gmail.com>
2018-07-07 11:53:46 +0530

    finding the duplicate char in char array

---
+++ src/CharCountInArray.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharCountInArray {
+
+    public static void main(String[] args) {
+
+        String string = "aaabbbcccdddeeefff";
+
+        // converting string to char array
+        char[] charArray = string.toCharArray();
+
+        // creating the map object.
+        Map<Character, Integer> map = new HashMap<>();
+
+        // Iterating the char array.
+
+
+        // iterating the array using foreach loop.
+        for (char key : charArray) {
+
+
+            // using for loop
+// ***********************************************************************************
+      /*      // getting the char at position i
+                        for (int i = 0; i < charArray.length; i++) {
+
+                // getting the char at position i
+                char key = charArray[i];             */
+
+//************************************************************************************
+
+            // checking char is present in the map or not
+            if (map.containsKey(key)) {
+
+                // if present get the count of the char
+                int count = map.get(key);
+
+                // and push the count value by plus one
+                map.put(key, ++count);
+
+            } else {
+
+                // if char is already not in map push it with count value one
+                map.put(key, 1);
+            }
+        }
+
+        // printing the char and number of count
+        for (Map.Entry obj : map.entrySet()) {
+            System.out.println(" Char :  " + obj.getKey() + "  " + "Number :  " + obj.getValue());
+        }
+    }
+}
