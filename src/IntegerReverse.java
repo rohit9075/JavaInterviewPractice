9b35c00f2714d653fe7d73bf075dbd1dccd32e27
Rohit Kumar <rohit9075@gmail.com>
2018-08-06 15:36:08 +0530

    Integer reverse

---
+++ src/IntegerReverse.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+/*
+Java code to reverse the integer number.
+ */
+
+public class IntegerReverse {
+
+
+    public static void main(String[] args) {
+
+        int num;
+        int reversed = 0;
+
+        Scanner in = new Scanner(System.in);
+
+        // getting the input number.
+        System.out.println("Please enter the number to reverse");
+        num = in.nextInt();
+
+
+        // iterating the loop till 0
+        while (num != 0) {
+
+            int digit = num % 10;
+
+            reversed = (reversed * 10) + digit;
+
+            num = num / 10;
+
+        }
+
+        // printing the output as reversed number.
+        System.out.println("Reversed number is : " + reversed);
+    }
+
+}
