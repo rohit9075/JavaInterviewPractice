c4fa3ab206d4b736a23638d58f2c5dc87476474b
Rohit Kumar <rohit9075@gmail.com>
2018-08-07 22:47:13 +0530

    comment

--- src/fibonacci_series/FibonacciSeries.java
+++ src/fibonacci_series/FibonacciSeries.java
@@ -1,9 +1,11 @@
 package fibonacci_series;
 
+/*
+Java class to find the FibonacciSeries to the given number.
+ */
 public class FibonacciSeries {
     // Driver main method to run the program
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         // Variable declaration and Initialization
         int mFirstNumber=0,mSecondNumber =1, mThirdNumber ,i,count=10;
 
@@ -11,8 +13,7 @@ public class FibonacciSeries {
         System.out.print(mFirstNumber+" "+mSecondNumber);
 
         //loop starts from 2 because 0 and 1 are already printed
-        for(i=2;i<count;++i)
-        {
+        for(i=2; i<count; ++i) {
             // Calculating third number by adding first two number
             mThirdNumber = mFirstNumber + mSecondNumber;
 
