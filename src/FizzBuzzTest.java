99474e3c6cb452123cba3f362f94f9be8198aab4
Rohit Kumar <rohit9075@gmail.com>
2018-08-09 17:08:03 +0530

    Fizz Buzz problem comments

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -11,9 +11,16 @@ public class FizzBuzzTest {
 
         // iterating the number using the for loop
         for (int i = 1; i <= 50; i++) {
-            if (i % (3 * 7) == 0) System.out.println("FizzBuzz");
-            else if (i % 7 == 0) System.out.println("Buzz");
+            // checking the number is divisible by both 3 and 5
+            if (i % (3 * 5) == 0) System.out.println("FizzBuzz");
+
+                // checking whether the number is divisible by 5
+            else if (i % 5 == 0) System.out.println("Buzz");
+
+                // Checking whether the number is divisible by 3
             else if (i % 3 == 0) System.out.println("Fizz");
+
+                // if number is not divisible by 3 and 5 then print the number as it is
             else System.out.println(i);
         }
     }
