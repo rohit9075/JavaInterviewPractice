2e4627681f2b8f96c41e1bd8a5543056effb753c
Rohit Kumar <rohit9075@gmail.com>
2018-08-09 17:06:12 +0530

    Fizz Buzz problem comments

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -1,6 +1,15 @@
+
+/*
+   Simple java class to print fizz buzz string according to logic.
+    */
+
 public class FizzBuzzTest {
+
+    // Driver main method
+
     public static void main(String args[]) {
 
+        // iterating the number using the for loop
         for (int i = 1; i <= 50; i++) {
             if (i % (3 * 7) == 0) System.out.println("FizzBuzz");
             else if (i % 7 == 0) System.out.println("Buzz");
