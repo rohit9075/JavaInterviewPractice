676a01c786570b86040f3dd00d20da906aec8403
Rohit Kumar <rohit9075@gmail.com>
2018-08-11 10:32:44 +0530

    Formatted output

---
+++ src/FormattedOutput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FormattedOutput {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int num = scan.nextInt();
+
+        // printing the int value as it is
+        System.out.printf("Simple integer x =  %d%n ", num);
+    }
+}
