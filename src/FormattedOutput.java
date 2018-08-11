d9ce069949af305b894abf1e7b793e51032407cf
Rohit Kumar <rohit9075@gmail.com>
2018-08-11 10:38:55 +0530

    Formatted output

--- src/FormattedOutput.java
+++ src/FormattedOutput.java
@@ -5,17 +5,18 @@ public class FormattedOutput {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Enter the number");
-        int num = scan.nextInt();
+//        System.out.println("Enter the number");
+//        int num = scan.nextInt();
 
+        int num = 100;
         // printing the int value as it is
         System.out.printf("Simple integer x =  %d%n ", num);
 
         // this will print it upto 2 decimal places
         System.out.printf("Formatted with precision: PI = %.2f%n", Math.PI);
 
-        double d = 100.20;
+        float d = 100.20f;
         // automatically appends zero to the rightmost part of decimal
-        System.out.printf("Formatted to specific width: n = %.4f\n", d);
+        System.out.printf("Formatted to specific width: n = %.4f%n", d);
     }
 }
