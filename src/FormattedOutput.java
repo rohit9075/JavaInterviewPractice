62ea0c016c386951ce79854702a685e9d8b2acca
Rohit Kumar <rohit9075@gmail.com>
2018-08-11 10:43:44 +0530

    Number format using decimal class

--- src/FormattedOutput.java
+++ src/FormattedOutput.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class FormattedOutput {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+//        Scanner scan = new Scanner(System.in);
 
 //        System.out.println("Enter the number");
 //        int num = scan.nextInt();
@@ -18,5 +18,19 @@ public class FormattedOutput {
         float d = 100.20f;
         // automatically appends zero to the rightmost part of decimal
         System.out.printf("Formatted to specific width: n = %.4f%n", d);
+
+        // here number is formatted from right margin and occupies a
+        // width of 20 characters
+        System.out.printf("Formatted to right margin: n = %20.4f\n", d);
+
+
+        /* **************  Formatting using DecimalFormat class **************************** */
+
+        double num1 = 123.4567;
+
+        // prints only numeric part of a floating number
+        DecimalFormat ft = new DecimalFormat("####");
+        System.out.println("Without fraction part: num = " + ft.format(num1));
+
     }
 }
