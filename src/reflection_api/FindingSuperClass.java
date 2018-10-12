5224c2c9ba4c873ac6d8e17e86ef59e05bc60989
Rohit Kumar <rohit9075@gmail.com>
2018-10-12 10:18:07 +0530

    Reflection Api

---
+++ src/reflection_api/FindingSuperClass.java
@@ -0,0 +1,18 @@
+package reflection_api;
+
+public class FindingSuperClass {
+
+    public static void main(String[] args) {
+
+        // instantiating the o with child class constructor
+        Object o = new FindingSuperClass();
+
+        // getting the class name
+        Class<?> c = o.getClass();
+        System.out.println("Class name is : " + c.getSimpleName());
+
+        // getting the super class name
+        Class<?> sup = c.getSuperclass();
+        System.out.println("Class name is : " + sup.getSimpleName());
+    }
+}
