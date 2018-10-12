d0ab12ee87b10dd7a0f3d094069570af59d54724
Rohit Kumar <rohit9075@gmail.com>
2018-10-12 10:07:59 +0530

    Reflection Api

--- src/reflection_api/Test.java
+++ src/reflection_api/Test.java
@@ -16,23 +16,32 @@ public class Test {
 
         Test test = new Test("Rohit", "Bangalore");
 
-        //
+        // getting the object reference of test class
         Class<?> obj = test.getClass();
 
+        // printing the fully qualified class name and simple class name;
         System.out.println(obj.getClass());
         System.out.println(obj.getSimpleName());
 
+        // getting the list of the constructor
         Constructor<?>[] constructors = obj.getConstructors();
 
+        // printing the number of constructor
         System.out.println("Number of constructores  : " + constructors.length);
 
+        // getting the construction at first position
         Constructor<?> con = constructors[0];
 
 
+        // creating the object of the Object Class.
         Object object;
 
         try {
+
+            //  Instantiating the object using test constructor
             object = con.newInstance("vivek kumar", "Dhawangere");
+
+            // printing the object.
             System.out.println(object);
         } catch (InstantiationException e) {
             e.printStackTrace();
