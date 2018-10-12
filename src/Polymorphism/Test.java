ad0f687e12b9eda0a25f57fad4aecc06c9df2582
Rohit Kumar <rohit9075@gmail.com>
2018-10-12 09:06:26 +0530

    Enum Example

--- src/Polymorphism/Test.java
+++ src/Polymorphism/Test.java
@@ -10,7 +10,9 @@ public class Test extends FactoryMethod {
 
         // scanner obj to get the user input
         Scanner in = new Scanner(System.in);
+
         System.out.println("Please enter you bank name : ");
+
         String bankName = in.nextLine();
 
         Bank bank = getObject(bankName);
