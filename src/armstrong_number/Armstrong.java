d457cbfbd9bc2abc4ec4b45d682b5aa95e0e4c23
Rohit Kumar <rohit9075@gmail.com>
2018-10-22 08:49:13 +0530

    getting the super class name and package name

--- src/armstrong_number/Armstrong.java
+++ src/armstrong_number/Armstrong.java
@@ -8,8 +8,10 @@ public class Armstrong {
         temp = n;
         while (n > 0) {
             a = n % 10;
-            n = n / 10;
+
             c = c + (a * a * a);
+
+            n = n / 10;
         }
         if (temp == c)
             System.out.println("armstrong number");
